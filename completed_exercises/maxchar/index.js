e5e734c94f3adf6bfced6a06b497b4ea0d2a9b08
Darren Suroviec <dcsuroviec@gmail.com>
2020-12-14 15:25:35 -0500

    update

--- completed_exercises/maxchar/index.js
+++ completed_exercises/maxchar/index.js
@@ -5,27 +5,50 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+//Darrens personal solutuion using Lodash
+
 function maxChar(str) {
-  const charMap = {};
-  let max = 0;
-  let maxChar = '';
+    let max = 0;
+    let char = "";
+    // convert to array to use Lodash...dont't need to split the array which is crazy
+    // let split = str.split("");
+    let obj = _.countBy(str);
 
-  for (let char of str) {
-    if (charMap[char]) {
-      charMap[char]++;
-    } else {
-      charMap[char] = 1;
+    for (let key in obj) {
+        if (obj[key] > max) {
+            max = obj[key];
+            char = key;
+        }
     }
-  }
-
-  for (let char in charMap) {
-    if (charMap[char] > max) {
-      max = charMap[char];
-      maxChar = char;
-    }
-  }
-
-  return maxChar;
 }
+maxChar("abddddddddeeeeeeeeeeedbcccc");
+
+// Grider's Solution
+
+// function maxChar(str) {
+//   const charMap = {};
+//   let max = 0;
+//   let maxChar = '';
+
+//   for (let char of str) {
+//     if (charMap[char]) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//     console.log(charMap)
+//   }
+
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//       max = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+
+//   return maxChar;
+// }
+
+maxChar("abddddddddeeeeeeeeeeedbcccc");
 
 module.exports = maxChar;
